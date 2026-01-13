e9ece61dc261a8e110bc1b39359068fa8c6c97b4
Somto Anazodo <your-email@example.com>
2026-01-13 12:39:46 +0100

    done

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,3 +1,3 @@
 // Use this file to configure the base URL for your backend API.
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:4000/api/';
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://vtserver-fk5vf.ondigitalocean.app/api/';
 
